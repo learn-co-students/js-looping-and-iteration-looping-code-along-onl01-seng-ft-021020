c6220454b444081bd24f3cadc27354e6d61bdb71
biagioo <mendolia2328@gmail.com>
2020-05-12 15:25:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+ const cards = ["Ada", "Brendan", "Ali"];
+ 
+function writeCards(cards, surprise) {
+  let thankYous = []
+    for (let i = 0; i < cards.length; i++) {
+    thankYous.push(`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
+  }
+  return thankYous;
+}
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--)
+    } 
+}
