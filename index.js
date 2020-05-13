dad53b142b792e338f04e76a5ff72e58cc2c9688
93geraldtee <93geraldtee@gmail.com>
2020-05-13 15:55:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    const messageArray = []
+    for (let i = 0; i < name.length; i++) {
+    messageArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messageArray
+}
+
+function countDown(integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer -= 1;
+    }
+    console.log(integer)
+}
