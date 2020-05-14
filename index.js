5772bf931f1f860ed09d6d7aabb2c3277a2e79cb
ktalmage <ktalmage85@gmail.com>
2020-05-14 12:27:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const nameList = [ 'Lisa', 'Kaitlin', 'Jan' ];
+const event = "surprise";
+// const messages = 
+
+function writeCards(nameList,event) {
+    
+     const newAr =  nameList.map( function(name){
+        return (`Thank you, ${name}, for the wonderful ${event} gift!`)
+    })
+    return newAr
+    }
+
+let i = 0;
+
+function countDown(i) {
+    for (i = 10; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
+
