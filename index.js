bcab568e3a5bade6f80eb15a8a64265609e80aef
kevinkim <kevinkim.yhk@gmail.com>
2020-05-13 21:49:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, occasion){
+    let array = []
+    for(let i=0; i<names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return array;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num --;
+    }
+  }
